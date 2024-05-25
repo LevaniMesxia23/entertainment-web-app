1e2ab3f722d06e61b44d5cffff6ca93e7f97e4f0
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-05-25 22:45:58 +0400

    style: import and add hook-form

--- src/components/LoginFormPage.tsx
+++ src/components/LoginFormPage.tsx
@@ -1,10 +1,17 @@
 import React, { useContext } from 'react';
 import Movie from "../../public/starter-code/assets/Movie.svg"
 import { Mycontext } from '../App'
+import { useForm } from 'react-hook-form';
 
 
 function FormPage() {
   const context = useContext(Mycontext)
+  const {register, handleSubmit, formState: {errors}} = useForm()
+
+  const onsubmit = async (data: {}) => {
+    console.log(data);
+    
+  }
 
   if (!context) {
     throw new Error("FormPage must be used within a MycontextProvider");
@@ -16,15 +23,15 @@ function FormPage() {
 
       <div className='bg-[#161D2F] w-[100%] rounded-[0.625rem] flex flex-col px-[1.5rem] pt-[1.5rem] pb-[2rem]'>
       <span className='text-white text-[2rem] tracking-[-0.03125rem]'>Login</span>
-      <div className='flex justify-center flex-col gap-6 my-10'>
-        <input type="email" placeholder='Email address' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F]'/>
-        <input type="password" placeholder='Password' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F]'/>
-      </div>
+      <form onSubmit={handleSubmit(onsubmit)} className='flex justify-center flex-col gap-6 my-10'>
+        <input {...register("email")} type="email" id='email' placeholder='Email address' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F] text-white'/>
+        <input {...register("password")} type="password" id='password' placeholder='Password' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F] text-white'/>
+      <button type='submit' className=' bg-[#FC4747] text-white rounded-[0.375rem] text-[0.9375rem] font-normal py-[0.90rem] mt-4'>Login to your account</button>
+      </form>
       
-      <button className=' bg-[#FC4747] text-white rounded-[0.375rem] text-[0.9375rem] font-normal py-[0.90rem] mb-6'>Login to your account</button>
       <div className='flex justify-center gap-[0.56rem]'>
       <span className='text-white text-[0.9375rem]'>Don’t have an account?</span>
-      <span className='text-[#FC4747] text-[0.9375] cursor-pointer'  onClick={() => setShow(!show)}>Sign Up</span>
+      <span className='text-[#FC4747] text-[0.9375] cursor-pointer' onClick={() => setShow(!show)}>Sign Up</span>
       </div>
       </div>
     </div>
--- src/components/SignUpFormPage.tsx
+++ src/components/SignUpFormPage.tsx
@@ -16,12 +16,12 @@ function SignUpFormPage() {
       <div className='bg-[#161D2F] w-[100%] rounded-[0.625rem] flex flex-col px-[1.5rem] pt-[1.5rem] pb-[2rem]'>
       <span className='text-white text-[2rem] tracking-[-0.03125rem]'>Sign Up</span>
       <div className='flex justify-center flex-col gap-6 my-10'>
-        <input type="email" placeholder='Email address' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F]'/>
-        <input type="password" placeholder='Password' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F]'/>
-        <input type="password" placeholder='Repeat Password' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F]'/>
+        <input type="email" placeholder='Email address' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F] text-white'/>
+        <input type="password" placeholder='Password' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F] text-white'/>
+        <input type="password" placeholder='Repeat Password' className='bg-transparent opacity-50 border-b-[0.0625rem] pl-4 pb-[1rem] border-[#5A698F] text-white'/>
+      <button className=' bg-[#FC4747] text-white rounded-[0.375rem] text-[0.9375rem] font-normal py-[0.90rem] mt-4'>Create an account</button>
       </div>
       
-      <button className=' bg-[#FC4747] text-white rounded-[0.375rem] text-[0.9375rem] font-normal py-[0.90rem] mb-6'>Create an account</button>
       <div className='flex justify-center gap-[0.56rem]'>
       <span className='text-white text-[0.9375rem]'>Already have an account?</span>
       <span className='text-[#FC4747] text-[0.9375] cursor-pointer' onClick={() => setShow(!show)}>Login</span>
